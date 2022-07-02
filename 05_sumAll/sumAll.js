2e3f6264b5b43acb62562859bfb13313be1e256e
Mahmoud <barghoutmahmoud2@gmail.com>
2022-07-02 20:29:24 +0200

    collective answer forgot to commit earlier

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(int1,int2) {
+let sum=0;
+let lowerLimit;
+let higherLimit;
+if((int1<0)||(int2<0)||(!Number.isInteger(int1))||(!Number.isInteger(int2)))
+{
+    return 'ERROR';
+}
 
+if (int1<=int2)
+{
+lowerLimit=int1;
+higherLimit=int2;
+}
+else
+{
+    lowerLimit=int2;
+    higherLimit=int1;
+}
+for(let i=lowerLimit;i<=higherLimit;i++)
+{
+    sum+=i;
+}
+return sum;
 };
 
 // Do not edit below this line
